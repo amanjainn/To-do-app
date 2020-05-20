1b26632cb31bb7c793874718d67f43984d2dfccf
Aman <amangrand@gmail.com>
2020-05-20 13:53:57 +0530

    Updates

---
+++ precache-manifest.3ae9753a32f157ad1d278670de0a3c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5895e127568c86fa80e50a87e9e57f6b",
+    "url": "/To-do-app/index.html"
+  },
+  {
+    "revision": "037470474f04089c29fe",
+    "url": "/To-do-app/static/css/main.954520c8.chunk.css"
+  },
+  {
+    "revision": "05bd3c7c8d6b59b5d510",
+    "url": "/To-do-app/static/js/2.a4ad7195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app/static/js/2.a4ad7195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037470474f04089c29fe",
+    "url": "/To-do-app/static/js/main.a220b437.chunk.js"
+  },
+  {
+    "revision": "431abac316aa22c497d7",
+    "url": "/To-do-app/static/js/runtime-main.57e96e56.js"
+  }
+]);
